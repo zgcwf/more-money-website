0df233c435a5ff584db90096c6e91d071792944b
zgc <1935083614@qq.com>
2021-09-21 00:29:48 +0800

    deploy

---
+++ precache-manifest.24687f0c0491e4b58e6fb210662b162f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102f449b8b001994c2a8",
+    "url": "/more-money-website/css/app.c57de4ad.css"
+  },
+  {
+    "revision": "92f13c9b69de8c2f73b491cc588558e7",
+    "url": "/more-money-website/index.html"
+  },
+  {
+    "revision": "102f449b8b001994c2a8",
+    "url": "/more-money-website/js/app.4874c1f0.js"
+  },
+  {
+    "revision": "e5bd62487808b55a571f",
+    "url": "/more-money-website/js/chunk-vendors.458a577a.js"
+  },
+  {
+    "revision": "42bfa4ba9edfb3426e53b3524294dc7f",
+    "url": "/more-money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/more-money-website/robots.txt"
+  }
+]);
